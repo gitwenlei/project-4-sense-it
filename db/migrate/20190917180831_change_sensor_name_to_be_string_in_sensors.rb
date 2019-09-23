5b50f4917fed8f1fbf1c2ad585a08a3167e94995
gitwenlei <ngwenlei@gmail.com>
2019-09-24 04:10:28 +0800

    deleted migration files for earlier alteration of tables

--- db/migrate/20190917180831_change_sensor_name_to_be_string_in_sensors.rb
+++
@@ -1,5 +0,0 @@
-class ChangeSensorNameToBeStringInSensors < ActiveRecord::Migration[5.2]
-  def change
-    change_column :sensors, :sensor_name, :string
-  end
-end
--- db/migrate/20190917182900_remove_log_time_from_logs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLogTimeFromLogs < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :logs, :log_time, :time
-  end
-end
